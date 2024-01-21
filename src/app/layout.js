4e596f868d56dabd5df21138dbe1f917015ac9c5
shprintsin <shnyor360@gmail.com>
2024-01-21 21:58:28 +0200

    inject fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,11 @@ import MainNav from "./layout/layout/header/Mainbar/MainNav/MainNav";
 import SideBar from "./layout/layout/sidebar/SideBar";
 import styles from "./layout/layout/Layout.module.css";
 import getData from "./utils/getData";
+import { Inter } from 'next/font/google'
+ import { Secular_One } from "next/font/google";
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] })
+ 
 
 
 export const viewport = {
@@ -16,15 +21,20 @@ export const viewport = {
   // Also supported by less commonly used
   // interactiveWidget: 'resizes-visual',
 }
-
+const font = Secular_One({ subsets: ['latin'] ,
+weight: '400',})
 export default async function RootLayout({ children }) {
   const data = await getData("layout")
   return (
     <html lang="he" dir="rtl">
       <head>
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-
+      <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700&display=swap" rel="stylesheet"></link>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet"></link>
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet"></link>
         <title>הפורום להשתלבות</title>
+   
       </head>
       <body >
       <div className={styles.container}>
